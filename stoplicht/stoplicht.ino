88b5df10107e55dba76fdd5f664732f0b5fcef41
Steven <s.koerts2@gmail.com>
2017-11-10 16:41:19 +0100

    stoplicht first try

---
+++ stoplicht/stoplicht.ino
@@ -0,0 +1,35 @@
+int trig = 13;
+int echo = 12;
+int redled = 11;
+int purpleled = 10;
+
+long duration;
+long distance;
+
+void setup(){
+  
+  pinMode(trig, OUTPUT); 
+  
+  pinMode(echo, INPUT);
+  
+  pinMode(redled, OUTPUT);
+ 
+  pinMode(purpleled, OUTPUT);
+}
+
+void loop(){
+digitalWrite(trig, LOW);
+delay(2);
+digitalWrite(trig,HIGH);
+
+duration = pulseIn(echo,HIGH);
+distance = duration * 0.034/2;
+if(distance < 4){
+ digitalWrite(purpleled,LOW);
+ digitalWrite(redled,HIGH);
+  }else{
+     digitalWrite(purpleled,HIGH);
+     digitalWrite(redled,LOW);
+    }
+}
+
