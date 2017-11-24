aa53013f527ada6948cbb4e073d6cf986d0f783b
Steven <s.koerts2@gmail.com>
2017-11-24 20:16:27 +0100

    new try stoplicht

--- stoplicht/stoplicht.ino
+++ stoplicht/stoplicht.ino
@@ -1,35 +1,63 @@
-int trig = 13;
-int echo = 12;
-int redled = 11;
-int purpleled = 10;
+//stoplicht 1
+int red1 = 7;
+int orng1 = 6;
+int grn1 = 5;
 
-long duration;
-long distance;
+// stoplicht 2
+int red2 = 11;
+int orng2 = 12; 
+int grn2 = 13;
 
-void setup(){
+//Ultrasonesensor
+int trig = 9;
+int echo = 10;
+int distance;
+int duration;
+
+void setup() {
+//setup stoplicht 1
+pinMode(red1, OUTPUT);
+pinMode(orng1, OUTPUT);
+pinMode(grn1, OUTPUT);
+//setup stoplicht 2
+pinMode(red2, OUTPUT);
+pinMode(orng2, OUTPUT);
+pinMode(grn2, OUTPUT);
+//setup ultrasone
+pinMode(trig, OUTPUT);
+pinMode(echo, INPUT);
+
+digitalWrite(grn1, HIGH);
+delay(100);
+digitalWrite(red2, HIGH);
   
-  pinMode(trig, OUTPUT); 
-  
-  pinMode(echo, INPUT);
-  
-  pinMode(redled, OUTPUT);
- 
-  pinMode(purpleled, OUTPUT);
+
 }
 
-void loop(){
-digitalWrite(trig, LOW);
-delay(2);
-digitalWrite(trig,HIGH);
+int checkTraffic(){
+  digitalWrite(trig,HIGH);
+  delay(100);
+  digitalWrite(trig,LOW);
+  duration = pulseIn(echo,HIGH);
+  distance = duration * 0.034/2;
 
-duration = pulseIn(echo,HIGH);
-distance = duration * 0.034/2;
-if(distance < 4){
- digitalWrite(purpleled,LOW);
- digitalWrite(redled,HIGH);
-  }else{
-     digitalWrite(purpleled,HIGH);
-     digitalWrite(redled,LOW);
-    }
+ return(distance);
+ }
+
+int switchLed(int led, int timeout){
+  delay(timeout);
+  if(digitalRead(led) == HIGH){
+    digitalWrite(led, LOW);
+    }else{
+      digitalWrite(led, HIGH);
+      }
+  }
+
+void loop() {
+   switchLed(grn1, 5000);
+   switchLed(orng1, 1000);
+   
+   switchLed(red1, 1000);
+   switchLed(orng1, 1000);
+  switchLed(red1, 5000);
 }
-
